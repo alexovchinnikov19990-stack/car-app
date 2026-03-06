f4471e53bf4a01dd888f65fa8f45af82ead58045
alexovchinnikov19990-stack <alexovchinnikov19990@gmail.com>
2026-03-06 10:25:36 +0300

    Add backup export and import functions
    Implement functions to export and import backup data.

---
+++ domain/backup.js
@@ -0,0 +1,39 @@
+import { state } from "../state/state.js";
+
+export function exportBackup(){
+
+    const data = JSON.stringify(state, null, 2);
+
+    const blob = new Blob([data], {type:"application/json"});
+
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+
+    a.href = url;
+    a.download = "belgee_backup.json";
+
+    a.click();
+
+}
+
+export function importBackup(file){
+
+    const reader = new FileReader();
+
+    reader.onload = function(){
+
+        const data = JSON.parse(reader.result);
+
+        localStorage.setItem(
+            "car_app_state",
+            JSON.stringify(data)
+        );
+
+        location.reload();
+
+    };
+
+    reader.readAsText(file);
+
+}
