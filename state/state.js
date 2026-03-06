772781c0b66a80cc6cef74c212d28cc875497cc9
alexovchinnikov19990-stack <alexovchinnikov19990@gmail.com>
2026-03-06 10:52:41 +0300

    Refactor state object to use let instead of export

--- state/state.js
+++ state/state.js
@@ -1,17 +1,7 @@
-export const state = {
+let state = {
 
-    car: {
-        name: "Belgee X50",
-        startMileage: 5
-    },
-
-    fuel: [],
-    service: [],
-    other: [],
-
-    meta: {
-        version: "8.0",
-        lastUpdate: null
-    }
+fuel:[],
+service:[],
+other:[]
 
 }
